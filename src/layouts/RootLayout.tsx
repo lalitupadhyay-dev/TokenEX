47e8bb64542e147b065d5d5b306ee565749f8e14
lalitupadhyay-dev <ulalit498@gmail.com>
2026-02-14 20:30:09 +0530

    TabLayout setup done

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
     return (
         <SafeAreaProvider>
-            <Stack>
+            <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="(tabs)" />
                 {/* <Stack.Screen name="" /> */}
             </Stack>
